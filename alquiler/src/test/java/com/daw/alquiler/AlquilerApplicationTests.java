9ef30ee5e49d4baeeaabfc5868a5781019c5b4e6
Emilio153 <eordcru000@g.educaand.es>
2025-12-08 20:54:05 +0100

    Creacion

---
+++ alquiler/src/test/java/com/daw/alquiler/AlquilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.alquiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
